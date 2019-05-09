c57c809e30c1092d37fcf9bfe0db5c860bc5f0f5
Adam Ehven <aehven@binarytrees.biz>
2019-05-09 10:20:35 -0600

    testing no customers and no guests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ PaperTrail.enabled = false
 User.create!(first_name: "Roger", last_name: "Waters", email: "admin@null.com", password: "password", role: "admin")
 
 # customer with a sub
-broomsticks = Customer.create!(name: "The Three Broomsticks", address1: "Somewhere in Hogsmeade")
+broomsticks = Customer.create!(name: "The Three Broomsticks", address1: "Somewhere in Hogsmeade") if ENV['USERS_BELONG_TO_CUSTOMERS'] == 'true'
 
 # supervisor in customer with sub
 User.create!(first_name: "Dani", last_name: "Litani", email: "supervisor_c_w_s@null.com", password: "password", role: "supervisor", customer: broomsticks)
@@ -17,21 +17,20 @@ User.create!(first_name: "Bryan", last_name: "Adams", email: "manager_c_w_s@null
 User.create!(first_name: "Edie", last_name: "Brickell", email: "regular_c_w_s@null.com", password: "password", role: "regular", customer: broomsticks)
 
 # regular in sub
-brewery = Customer.create!(name: "The Brewery", address1: "Somewhere else in Hogsmeade")
+brewery = Customer.create!(name: "The Brewery", address1: "Somewhere else in Hogsmeade") if ENV['USERS_BELONG_TO_CUSTOMERS'] == 'true'
 User.create!(first_name: "Susanna", last_name: "Hoffs", email: "regular_s@null.com", password: "password", role: "regular", customer: brewery)
 
 # brewery.move_to_child_of broomsticks
 
 # manager in customer without sub
-cons = Customer.create!(name: "The Conservatory", address1: "Somewhere in Paris")
+cons = Customer.create!(name: "The Conservatory", address1: "Somewhere in Paris") if ENV['USERS_BELONG_TO_CUSTOMERS'] == 'true'
 User.create!(first_name: "Fred", last_name: "Chopin", email: "manager_c_wo_s@null.com", password: "password", role: "manager", customer: cons)
 
 # demo/anonymous users customer
-democ = Customer.find_or_create_by(name: "Demos")
-democ.update_attributes(address1: "Somewhere in Hogsmeade")
+democ = Customer.find_or_create_by(name: "Demos", address1: "Somewhere in Hogsmeade") if ENV['USERS_BELONG_TO_CUSTOMERS'] == 'true'
 demou = User.where(email: "demo@null.com").first
 if(demou)
-  demou.update_column(:customer_id, democ.id)
+  demou.update_column(:customer_id, democ&.id)
 else
   User.create!(first_name: "Brandi", last_name: "Carlile", email: "demo@null.com", password: "password", role: "regular", customer: democ)
 end
