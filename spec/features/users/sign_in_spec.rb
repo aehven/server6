38f18baac4304bd37ddafc52ebd9ad86d7ecc10d
Adam Ehven <aehven@binarytrees.biz>
2019-06-13 13:19:19 -0600

    test toc sign-in flow

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -6,6 +6,18 @@ feature "Signing In: " do
   let(:user) { FactoryBot.create(:user) }
   let(:home) { HomeComponent.new }
 
+  scenario "success" do
+    login_component.sign_in_creds user.email, "password"
+
+    expect(page).to have_css("h3", text: "Please agree to the terms and conditions below to proceed.")
+    expect(page).to have_text "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    expect(page).to have_text "Ipsum suspendisse ultrices gravida dictum. Quis risus sed"
+
+    click_on "Agree"
+
+    expect(page).to have_text "home works!"
+  end
+
   scenario "failure" do
     login_component.sign_in_creds user.email, "wrong password"
 
