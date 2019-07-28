1fe6eeedfcbe934c8601279116862475fa8fe7c5
Adam Ehven <aehven@binarytrees.biz>
2019-07-28 16:37:21 -0600

    notification private and public channels

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,13 +1,13 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
-    stream_for current_user
+    stream_from "notification:notifications_for_#{params[:user_id]}"
     notify_signed_in
   end
 
   def notify_signed_in
     logger.debug "NOTIFIY_SIGNED_IN"
     notification = Notification.new(title: "Sign-in", text: "Signed in as #{current_user.email}", level: :Info, in_app: true, duration: 3)
-    NotificationChannel.broadcast_to current_user, NotificationSerializer.new(notification).attributes
+    NotificationChannel.broadcast_to "notifications_for_#{current_user.id}", NotificationSerializer.new(notification).attributes
   end
 end
 # NotificationChannel.broadcast_to User.first, "hi"
