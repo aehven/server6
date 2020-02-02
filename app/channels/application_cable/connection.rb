c6cb7675a818687adbef671034d3c9ad70abe4dc
Adam Ehven <aehven@binarytrees.biz>
2020-02-02 16:54:18 -0700

    fix action cable authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,47 +1,20 @@
-# https://github.com/lynndylanhurley/devise_token_auth/issues/986#issuecomment-338290148
-# AND
-# https://github.com/lynndylanhurley/devise_token_auth/issues/986#issuecomment-355798258
+require 'jwt'
+
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user_wst
-      # `say "cable connected for #{current_user.first_name}"`
-      logger.add_tags 'ActionCable', current_user.email
+      jwt = JWT.decode(request.query_parameters[:jwt], Rails.application.secrets.secret_key_base, { algorithm: 'HS256' })
+      current_user = User.find((jwt[0]['sub']))
+      
+      if current_user
+        logger.add_tags 'ActionCable', current_user.email
+        logger.debug "CONNECTION Accepted"
+      else
+        logger.debug "CONNECTION Rejected"
+        reject_unauthorized_connection
+      end
     end
-
-    private
-      # https://github.com/lynndylanhurley/devise_token_auth/issues/986#issuecomment-338290148
-      def find_verified_user
-        logger.debug "CONNECTION request.query_parameters: #{request.query_parameters}"
-        accessToken = request.query_parameters[:accessToken]
-        uid = request.query_parameters[:uid]
-        client_id = request.query_parameters[:client]
-
-        user = User.find_by_email(uid)
-
-        if user && user.valid_token?(accessToken, client_id)
-          user
-        else
-          reject_unauthorized_connection
-        end
-      end
-
-      def find_verified_user_wst
-        logger.debug "CONNECTION request.query_parameters: #{request.query_parameters}"
-        uid = request.query_parameters[:uid]
-        websocket_token = request.query_parameters[:wst]
-
-        user = User.find_by(email: uid, websocket_token: websocket_token)
-
-        if user
-          logger.debug "CONNECTION Accepted"
-          user
-        else
-          logger.debug "CONNECTION Rejected"
-          reject_unauthorized_connection
-        end
-      end
   end
 end
