ad3433e4177969941ee8581417f0b830b64e0d44
Adam Ehven <aehven@binarytrees.biz>
2020-06-19 17:55:13 -0600

    some queries and mutations on User are working

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,17 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :users,
+      [Types::UserType],
+      null: false,
+      description: "Returns a list of all users"
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false
+    def user
+      User.first
     end
   end
 end
