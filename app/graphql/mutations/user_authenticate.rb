86e147612a6b225c302ce7adabb67c3b75416a11
Adam Ehven <aehven@binarytrees.biz>
2020-06-20 11:34:59 -0600

    Add error message to auth failure

--- app/graphql/mutations/user_authenticate.rb
+++ app/graphql/mutations/user_authenticate.rb
@@ -8,15 +8,17 @@ module Mutations
     def resolve(email: nil, password: nil)
       entity = User.find_by(email: email)
 
-      return unless entity.authenticate(password)
-
-      if entity.respond_to? :to_token_payload
-        @token = Knock::AuthToken.new payload: entity.to_token_payload
-      else
-        @token = Knock::AuthToken.new payload: { sub: entity.id }
+      if entity.authenticate(password)
+        if entity.respond_to? :to_token_payload
+          @token = Knock::AuthToken.new payload: entity.to_token_payload
+        else
+          @token = Knock::AuthToken.new payload: { sub: entity.id }
+        end
+  
+        { jwt: @token.token }
+        else
+          GraphQL::ExecutionError.new("User not found.");
       end
-
-      { jwt: @token.token }
     end
   end
 end
