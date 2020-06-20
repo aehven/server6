0e53a16d22d9a7974910e5bf047cf5f9519f8b38
Adam Ehven <aehven@binarytrees.biz>
2020-06-20 11:10:06 -0600

    Removed GraphiQL from server; using standalone 'brew install graphiql.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
 
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
