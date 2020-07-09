766bee3aacb15bc9fb916703451bc7c1026c7900
Adam Ehven <aehven@binarytrees.biz>
2020-07-09 14:03:19 -0600

    add mutation for Test; test.page can create and update tests

---
+++ app/graphql/mutations/test.rb
@@ -0,0 +1,19 @@
+module Mutations
+  class Test < BaseMutation
+    argument :id, ID, required: false
+    argument :name, String, required: true
+
+    type Types::TestType
+
+    def resolve(id: nil, name:)
+      raise CanCan::AccessDenied unless context[:ability].can? :create, ::Test
+
+      @test = id.nil? ? ::Test.new : ::Test.find(id)
+
+      @test.update_attributes(name: name)
+      @test.save
+
+      @test
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :test, mutation: Mutations::Test
     field :user_create, mutation: Mutations::UserCreate
     field :user_authenticate, mutation: Mutations::UserAuthenticate
   end
