c731aadd8677fdf0e2f712e5c5e7848dfb4ce69b
Adam Ehven <aehven@binarytrees.biz>
2020-07-29 13:37:20 -0600

    testplan saving tweaks

--- app/graphql/mutations/test_plan_mutation.rb
+++ app/graphql/mutations/test_plan_mutation.rb
@@ -11,15 +11,25 @@ module Mutations
 
       raise CanCan::AccessDenied unless context[:current_user].can? :create, TestPlan
 
-      @test_plan = (params[:id].to_i > 0) ? TestPlan.find(params[:id].to_i) : TestPlan.new
+      @test_plan = (params[:id].to_i > 0) ? TestPlan.find(params[:id].to_i) : TestPlan.new(name: params[:name])
 
-      @test_plan.update(params.slice(:name))
+      # @test_plan is a new record or an existing one.  If it's an existing one, it
+      # check if the name is changing; if it is, create a new one anyway, we
+      # don't change names, we create new plans.
+
+      if(!@test_plan.new_record? && @test_plan.name != params[:name])
+        @test_plan = TestPlan.new(name: params[:name])
+      end
+
       @test_plan.save
 
       @test_plan.tests = []
 
       params[:tests]&.each do |test|
-        TestPlansTest.create(test_id: test.testId, test_plan_id: @test_plan.id, duration: test.duration)
+        TestPlansTest.create(test_id: test.testId, 
+                             test_plan_id: @test_plan.id, 
+                             high_res: test.highRes,
+                             duration: test.duration)
       end
 
       @test_plan
--- app/graphql/types/test_plan_test_input.rb
+++ app/graphql/types/test_plan_test_input.rb
@@ -2,5 +2,6 @@ module Types
   class TestPlanTestInput < Types::BaseInputObject
     argument :testId, ID, required: true
     argument :duration, Integer, required: true
+    argument :highRes, Boolean, required: true
   end
 end
