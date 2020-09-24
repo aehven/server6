63d0f2d911071191150b88a4a562685d140b2cb0
Adam Ehven <aehven@binarytrees.biz>
2020-09-24 11:28:39 -0600

    Added patient mutation

---
+++ app/graphql/mutations/patient_mutation.rb
@@ -0,0 +1,31 @@
+module Mutations
+  class PatientMutation < BaseMutation
+    #####
+    #TODO consider splitting update and create into two separate mutations
+    #so that arguments can be required or optional in a stricter way.
+    #Right now, everything is optional because udpate might not need them all,
+    #and if ID is given then we force it to be an update.....
+    #####
+    argument :id, ID, required: false, description: "Must be included if updating an existing patient; omit for creating a patient."
+    argument :email, String, required: false, description: "Must be included for new patient creation."
+    argument :first_name, String, required: false, description: "Must be included for new patient creation."
+    argument :last_name, String, required: false, description: "Must be included for new patient creation."
+    argument :organization_id, ID, required: false
+
+    type Types::PatientType
+
+    def resolve(params={})
+      # raise CanCan::AccessDenied unless context[:current_patient].can?(:create, Patient)
+
+      if(params[:id].nil?)
+        @patient = Patient.create!(params)
+      else
+        @patient = Patient.find(params[:id])
+
+        @patient.update(params.slice(:first_name, :last_name, :email))
+      end
+
+      @patient
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,8 +3,12 @@ module Types
     field :cteData, mutation: Mutations::CteDataMutation
     field :cteDex, mutation: Mutations::CteDexMutation
     field :cteLog, mutation: Mutations::CteLogMutation
+
+    field :patient, mutation: Mutations::PatientMutation
+    
     field :test, mutation: Mutations::TestMutation
     field :testPlan, mutation: Mutations::TestPlanMutation
+    
     field :user, mutation: Mutations::UserMutation
     field :user_authenticate, mutation: Mutations::UserAuthenticate
   end
